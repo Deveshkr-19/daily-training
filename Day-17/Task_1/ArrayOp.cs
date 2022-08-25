3252b0984db713d8c8b88d7a2f9acab8b09b59b4
Deveshkr-19 <Devesh.Singh@bhavnacorp.com>
2022-08-25 13:06:33 +0530

    Day 17 task 1 submission

---
+++ Day-17/Task_1/ArrayOp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_1
+{
+    class ArrayOp
+    {
+        public ArrayOp()
+        {
+            Console.WriteLine("Object has been created");
+        }
+
+        public void print_array(int[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public void area(int x, int y)
+        {
+            Console.WriteLine(x * y);
+        }
+
+        public void area(double r)
+        {
+            Console.WriteLine(2 * 3.14 * r);
+        }
+    }
+}
---
+++ Day-17/Task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 10, 23, 45, 12, 65 };
+            int[] arrA = new int[arr.Length];
+            ArrayOp arrObj = new ArrayOp();
+
+            arrObj.print_array(arr);
+
+            Array.Sort(arr);
+            arrObj.print_array(arr);
+
+            Array.Reverse(arr);
+            arrObj.print_array(arr);
+
+            Array.Copy(arr, arrA, arr.Length);
+            arrObj.print_array(arr);
+
+            int l = 5, b = 3;
+            double r = 8;
+
+            arrObj.area(l, b);
+            arrObj.area(r);
+        }
+    }
+}
