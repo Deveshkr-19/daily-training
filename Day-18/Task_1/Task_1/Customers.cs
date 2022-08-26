1e70505a40076c1ab332d89166c90baec2803cc3
Deveshkr-19 <Devesh.Singh@bhavnacorp.com>
2022-08-26 16:01:38 +0530

    Day 18 task 1 submission

---
+++ Day-18/Task_1/Task_1/Customers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_1
+{
+    class Customers : Icommon
+    {
+        public int custID;
+        public string name, gender;
+
+        public void showDetails()
+        {
+            Console.WriteLine("Here are the details you entered");
+            Console.WriteLine(this.custID);
+            Console.WriteLine(this.name);
+            Console.WriteLine(this.gender);
+        }
+    }
+}
---
+++ Day-18/Task_1/Task_1/Products.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_1
+{
+    class Products : Icommon
+    {
+        public int prodID, quantity;
+        public string prodName, brand;
+        public void showDetails()
+        {
+            Console.WriteLine("Here are the details you entered");
+            Console.WriteLine(this.prodID);
+            Console.WriteLine(this.prodName);
+            Console.WriteLine(this.brand);
+            Console.WriteLine(this.quantity);
+        }
+    }
+}
