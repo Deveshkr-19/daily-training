f83973f9fb1ddc5315af67b0296da2d37098c77e
Deveshkr-19 <Devesh.Singh@bhavnacorp.com>
2022-09-14 17:59:46 +0530

    Day-32 Thread Practice

--- Assessments/Assessment 12-Sep/AutoMobile/Program.cs
+++ Assessments/Assessment 12-Sep/AutoMobile/Program.cs
@@ -9,7 +9,6 @@ namespace AutoMobile
     {
         static void Main(string[] args)
         {
-            // Connection strings
             SqlConnection con = new SqlConnection("server=localhost;database=cars_db;integrated security=true");
             DataSet ds = new DataSet();
 
@@ -29,7 +28,9 @@ namespace AutoMobile
                     "Enter 2 to view calculated salaries\n" +
                     "Enter 3 to calculate the manufacturing price and the purchase cost of the car\n" +
                     "Enter 4 to calculate repairing cost\n");
+
                 int choice = int.Parse(Console.ReadLine());
+
                 switch(choice)
                 {
                     case 1:
@@ -42,7 +43,7 @@ namespace AutoMobile
                         int working_hrs = 184;
 
                         SqlDataAdapter sal_da = new SqlDataAdapter("select e.emp_id, e.name, " +
-                            "d.dept_head, m.hours_worked, d.hrly_salary from Employee e " +
+                            "d.dept_head, m.hours_worked, d.hrly_salary, d.dept_name from Employee e " +
                             "Inner Join Monthly_hours m on e.emp_id = m.emp_id inner join " +
                             "Department d on e.dept_id = d.dept_id", con);
 
@@ -64,7 +65,8 @@ namespace AutoMobile
                             int hrs_worked = (int)ds.Tables["Hrs_worked Hrly_salary"].Rows[i][3];
 
                             total_salary = hrs_worked > working_hrs ?
-                                ((working_hrs * hrly_salary) + ((hrs_worked - working_hrs) * hrly_salary * 2)) : 
+                                ((working_hrs * hrly_salary) + 
+                                ((hrs_worked - working_hrs) * hrly_salary * 2)) : 
                                 (working_hrs * hrly_salary);
 
                             if (!File.Exists(salRecord))
@@ -72,14 +74,16 @@ namespace AutoMobile
                                 {
                                     sw.WriteLine($"Emp. ID: {ds.Tables["Hrs_worked Hrly_salary"].Rows[i][0]}\t/\t" +
                                         $"Name: {ds.Tables["Hrs_worked Hrly_salary"].Rows[i][1]}\t/\t" +
-                                        $"Salary: {total_salary}");
+                                        $"Department: {ds.Tables["Hrs_worked Hrly_salary"].Rows[i][5]}\t/\t" +
+                                        $"Salary: ₹ {total_salary}");
                                 }
                             else
                                 using (StreamWriter sw = File.AppendText(salRecord))
                                 {
                                     sw.WriteLine($"Emp. ID: {ds.Tables["Hrs_worked Hrly_salary"].Rows[i][0]}\t/\t" +
                                         $"Name: {ds.Tables["Hrs_worked Hrly_salary"].Rows[i][1]}\t/\t" +
-                                        $"Salary: {total_salary}");
+                                        $"Department: {ds.Tables["Hrs_worked Hrly_salary"].Rows[i][5]}\t/\t" +
+                                        $"Salary: ₹ {total_salary}");
                                 }
                         }
 
@@ -107,7 +111,6 @@ namespace AutoMobile
                             Console.WriteLine();
                         }
                         
-                        
                         break;
 
                     case 3:
@@ -116,6 +119,7 @@ namespace AutoMobile
 
                         SqlDataAdapter allParts_da = new SqlDataAdapter("select * from products", con);
                         allParts_da.Fill(ds, "All Parts Details");
+
                         int allPartsRowCount = ds.Tables["All Parts Details"].Rows.Count;
 
                         double manufacture_cost = 0;
@@ -125,6 +129,7 @@ namespace AutoMobile
                             if (check.CheckMultParts(ds.Tables["All Parts Details"].Rows[i][1].ToString(), MultiParts.fourParts))
                             {
                                 manufacture_cost += 4 * (double)ds.Tables["All Parts Details"].Rows[i][2];
+
                                 SqlCommand fourPartsStock = new SqlCommand($"update Stocks set quantity = quantity - 4 " +
                                     $"where prod_id = {ds.Tables["All Parts Details"].Rows[i][0]}", con);
 
@@ -134,6 +139,7 @@ namespace AutoMobile
                             else if (check.CheckMultParts(ds.Tables["All Parts Details"].Rows[i][1].ToString(), MultiParts.twoParts))
                             {
                                 manufacture_cost += 2 * (double)ds.Tables["All Parts Details"].Rows[i][2];
+
                                 SqlCommand twoPartsStock = new SqlCommand($"update Stocks set quantity = quantity - 2 " +
                                     $"where prod_id = {ds.Tables["All Parts Details"].Rows[i][0]}", con);
 
@@ -142,6 +148,7 @@ namespace AutoMobile
                             else
                             {
                                 manufacture_cost += (double)ds.Tables["All Parts Details"].Rows[i][2];
+
                                 SqlCommand onePartStock = new SqlCommand($"update Stocks set quantity = quantity - 1 " +
                                     $"where prod_id = {ds.Tables["All Parts Details"].Rows[i][0]}", con);
 
@@ -154,7 +161,7 @@ namespace AutoMobile
 
                         manufacture_cost += (35 * manufacture_cost) / 100;
 
-                        Console.WriteLine($"\nThe cost price of the car with 35% profit is: {manufacture_cost}\n");
+                        Console.WriteLine($"\nThe purchase cost of the car with 35% profit is: {manufacture_cost}\n");
 
                         break;
 
@@ -167,8 +174,10 @@ namespace AutoMobile
                             ds.Tables.Remove(ds.Tables["Parts Details"]);
 
                         int[] partsArr = new int[nParts];
+
                         SqlDataAdapter parts_da = new SqlDataAdapter("select * from products", con);
                         parts_da.Fill(ds, "Parts Details");
+
                         int partsRowCount = ds.Tables["Parts Details"].Rows.Count;
 
                         double repair_cost = nParts * 800;
@@ -191,6 +200,7 @@ namespace AutoMobile
                                 if((int)ds.Tables["Parts Details"].Rows[j][0] == partsArr[i])
                                 {
                                     repair_cost += (double)ds.Tables["Parts Details"].Rows[j][2];
+
                                     SqlCommand repStock = new SqlCommand($"update Stocks set quantity = quantity - 1 " +
                                         $"where prod_id = {partsArr[i]}", con);
 
---
+++ Day-32/ThreadingPractice/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadingPractice
+{
+    class Program
+    {
+        // ------------------------------- {3} Lock
+        static readonly object pblock = new object();
+        static void PrintInfo()
+        {
+            lock (pblock)
+            {
+                for (int i = 1; i <= 4; i++)
+                {
+                    Console.WriteLine("i value: {0}", i);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            //// ------------------------------- {1} Sleep thread
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    Console.WriteLine("Thread paused for {0} second", 1);
+            //    // Pause thread for 1 second
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("i value: {0}", i);
+            //}
+
+
+
+
+            //// ------------------------------- {2} Sleep thread with Timespan property
+            //TimeSpan ts = new TimeSpan(0, 0, 1);
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    Console.WriteLine("Thread paused for {0} second", 1);
+            //    // Pause thread for 1 second
+            //    Thread.Sleep(ts);
+            //    Console.WriteLine("i value: {0}", i);
+            //}
+            //Console.WriteLine("Main Thread Exists");
+
+
+
+            // ------------------------------- {3} Lock
+            Thread t1 = new Thread(new ThreadStart(PrintInfo));
+            Thread t2 = new Thread(new ThreadStart(PrintInfo));
+            t1.Start();
+            t2.Start();
+
+        }
+    }
+}
